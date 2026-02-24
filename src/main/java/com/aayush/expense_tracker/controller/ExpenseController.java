67fe6dc0ae3f64b468c019850ee8750101195706
aayush-verma8 <aayushverma.av2025@gmail.com>
2026-02-24 15:46:47 +0530

    feat: implemented JWT authentication filter and secured expense endpoints

--- src/main/java/com/aayush/expense_tracker/controller/ExpenseController.java
+++ src/main/java/com/aayush/expense_tracker/controller/ExpenseController.java
@@ -4,6 +4,7 @@ import com.aayush.expense_tracker.model.User;
 import com.aayush.expense_tracker.model.Expense;
 import com.aayush.expense_tracker.service.ExpenseService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,37 +22,45 @@ public class ExpenseController {
     @PostMapping
     public ResponseEntity<Expense> addExpense(
             @RequestBody Expense expense,
-            @RequestHeader("user-email") String email) {
+            Authentication authentication) {
 
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+        User user = (User) authentication.getPrincipal();
 
         Expense savedExpense = expenseService.addExpense(expense, user);
 
         return ResponseEntity.status(201).body(savedExpense);
     }
     @GetMapping
-    public ResponseEntity<List<Expense>> getUserExpennses(
-            @RequestHeader("user-email") String email){
-        User user = userRepository.findByEmail(email).orElseThrow(()-> new RuntimeException("User not found"));
-        List<Expense> expense=expenseService.getUserExpenses(user);
-        return ResponseEntity.ok(expense);
+    public ResponseEntity<List<Expense>> getUserExpenses(
+            Authentication authentication) {
+
+        User user = (User) authentication.getPrincipal();
+
+        List<Expense> expenses = expenseService.getUserExpenses(user);
+
+        return ResponseEntity.ok(expenses);
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteExpense(
             @PathVariable Long id,
-            @RequestHeader("user-email")String email){
-        User user = userRepository.findByEmail(email).orElseThrow(()->new RuntimeException("User not found"));
-        expenseService.deleteExpense(id,user);
+            Authentication authentication) {
+
+        User user = (User) authentication.getPrincipal();
+
+        expenseService.deleteExpense(id, user);
+
         return ResponseEntity.ok("Expense deleted successfully");
     }
     @PutMapping("/{id}")
     public ResponseEntity<Expense> updateExpense(
             @PathVariable Long id,
             @RequestBody Expense expense,
-            @RequestHeader("user-email") String email){
-        User user = userRepository.findByEmail(email).orElseThrow(()->new RuntimeException("User not found"));
-        Expense updated=expenseService.updateExpense(id,expense,user);
+            Authentication authentication) {
+
+        User user = (User) authentication.getPrincipal();
+
+        Expense updated = expenseService.updateExpense(id, expense, user);
+
         return ResponseEntity.ok(updated);
     }
 }
