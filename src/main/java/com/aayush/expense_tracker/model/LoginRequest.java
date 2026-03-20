e340462a38c8a377a8a1e682befe4571ad7ff4af
aayush-verma8 <aayushverma.av2025@gmail.com>
2026-03-20 15:15:51 +0530

    fix login request

--- src/main/java/com/aayush/expense_tracker/model/LoginRequest.java
+++ src/main/java/com/aayush/expense_tracker/model/LoginRequest.java
@@ -1,9 +1,25 @@
 package com.aayush.expense_tracker.model;
-import lombok.*;
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class LoginRequest {
+
     private String email;
     private String password;
+
+    // ✅ getters
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // ✅ setters
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
