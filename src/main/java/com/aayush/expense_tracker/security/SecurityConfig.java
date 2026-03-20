70c313b9b54bdbee285d58f84fb2a15436bc6ce9
aayush-verma8 <aayushverma.av2025@gmail.com>
2026-03-20 13:59:37 +0530

    fix: allow all endpoints

--- src/main/java/com/aayush/expense_tracker/security/SecurityConfig.java
+++ src/main/java/com/aayush/expense_tracker/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/**").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
